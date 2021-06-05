64536faf0f9f5de95a66bd648263df172be7e4b4
AHUI_ <AHUI_@LAPTOP-RS6DL6U8>
2021-06-05 17:04:16 -0500

    Servicio para crear usuarios y validacion de username y password. Close #5

--- Aplicacion/src/main/java/com/ahuigarBlog/aplicacion/service/UserService.java
+++ Aplicacion/src/main/java/com/ahuigarBlog/aplicacion/service/UserService.java
@@ -1,8 +1,12 @@
 package com.ahuigarBlog.aplicacion.service;
 
+import javax.validation.Valid;
+
 import com.ahuigarBlog.aplicacion.entity.User;
 
 public interface UserService {
 
 	public Iterable<User> getAllUsers();
+
+	public User createUser(User user) throws Exception;
 }
